4f45d33e5c378aeed2516bac2d851cce28d6271f
DESKTOP-DQJT87J\渡辺智之 <razgriz1998@outlook.jp>
2018-09-01 20:20:24 +0900

    タイトル画面仮完成（仮素材により、背景、スタートボタン、終了ボタンを追加）

---
+++ Assets/Scripts/Title_Scene/PushExitButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushExitButton : MonoBehaviour {
+
+    public void PushButton() {
+        Application.Quit();
+        Debug.Log("Application Quit");
+    }
+
+}
