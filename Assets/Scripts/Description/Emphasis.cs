e943b9a84b4f4b24fc156c8cf55d0acedb9ee60b
DESKTOP-DQJT87J\渡辺智之 <razgriz1998@outlook.jp>
2018-10-25 21:38:42 +0900

    チュートリアルの仮完成 未完成・・・本素材の適用、BGM、矢印の点滅アニメーション

---
+++ Assets/Scripts/Description/Emphasis.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emphasis : MonoBehaviour {
+
+    private float sumTime = 0.0f;
+    private float intervalTime = 0.2f;
+    void Update() {
+            sumTime += Time.deltaTime;
+            if (sumTime > 8.0) {
+                intervalTime += Time.deltaTime;
+            if (intervalTime >= 0.2f) {
+                intervalTime = 0.0f;
+                //gameObject.renderer.enabled = !gameObject.renderer.enabled;
+            }
+        }
+    }
+}
