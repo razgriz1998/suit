cde3f72b8a46cd1cefa778f1722a2ef010b3084a
DESKTOP-DQJT87J\渡辺智之 <razgriz1998@outlook.jp>
2018-11-19 08:54:33 +0900

    OPをシーンまたいで流れるよう変更 ゲーム中ポーズからタイトルへ戻るとプレイできなくなる問題を修正

---
+++ Assets/Scripts/Title_Scene/OPManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OPManager : MonoBehaviour {
+
+    static OPManager _instance = null;
+
+    static OPManager instance
+    {
+        get { return _instance ?? (_instance = FindObjectOfType<OPManager>()); }
+    }
+
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private float loopTime, endTime;
+
+    // Use this for initialization
+    void Start() {
+        if (this != instance)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (audioSource.isPlaying) {
+            if (audioSource.time >= endTime) {
+                audioSource.time = loopTime;
+            }
+        }
+
+        if (Application.loadedLevelName == "Game_Main") {
+            if (this == instance) {
+                _instance = null;
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
